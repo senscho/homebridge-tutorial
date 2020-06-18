124487de6fee4a94fb9d5e593d38df3dee942a35
senscho <analog.cho@gmail.com>
2020-06-17 17:13:34 -0700

    module skeleton

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+/*
+    run script in strict mode; i.e. not allowing
+    - use of undeclared var
+    - deleting var or obj
+    - deleting function
+    - etc....
+
+    I think most other languages just don't allow it.
+    JS without strict mode seems just too flexible.
+*/
+
+module.exports = function (homebridge) {
+};
